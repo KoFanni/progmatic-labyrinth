f9d99e0002b6951da5d1e82c9cdd2a3e7c7b8beb
KoFanni <fankov96@gmail.com>
2020-05-18 14:26:38 +0200

    loadLabyrinthFile, setSize, getWidth, getHeight, getCellType, setCellType, getPlayerPosition, hasPlayerFinished, possibleMoves and RandomPlayerImpl Done

---
+++ src/main/java/com/progmatic/labyrinthproject/RandomPlayerImpl.java
@@ -0,0 +1,13 @@
+package com.progmatic.labyrinthproject;
+
+import com.progmatic.labyrinthproject.enums.Direction;
+import com.progmatic.labyrinthproject.interfaces.Labyrinth;
+import com.progmatic.labyrinthproject.interfaces.Player;
+
+public class RandomPlayer implements Player {
+    @Override
+    public Direction nextMove(Labyrinth l) {
+        int randomMove = (int)(Math.random())*(l.possibleMoves().size()-1);
+        return l.possibleMoves().get(randomMove);
+    }
+}
